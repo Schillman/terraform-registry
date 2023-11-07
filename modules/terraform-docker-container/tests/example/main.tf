f142f82c45874b0ba6dc56c45aff3e8c4b52bf4d
Sebastian <sebastian+github@schillman.se>
2023-11-07 03:18:07 +0100

    feat: Add new Terraform module for Docker container provisioning (#3)
    * adding docker container module.
    
    * adding docker container module.
    
    * added tests
    
    * fixed variable declaration and test
    
    * fixed variable declaration and readme
    
    * fixed linting
    
    * Update README.md
    
    * added new line after usage

---
+++ modules/terraform-docker-container/tests/example/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+}
+
+
+module "ubuntu" {
+  source = "../.."
+
+  name              = format("server-%s", "tst")
+  docker_image_name = "ubuntu"
+  network_mode      = "bridge"
+
+  environment_variables = ["KEY1=VALUE1", "KEY2=VALUE2"]
+
+  ports = [{
+    internal = 22
+    external = 22
+  }]
+
+  volumes = {
+    "Files" = {
+      name           = "Files"
+      container_path = "/Files"
+    }
+  }
+}
